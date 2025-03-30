40f1da1322fc226ef58ddc3804c6f453c7307887
srideep-banerjee <banerjee.srideep@gmail.com>
2025-03-30 10:06:50 +0530

    Improved concurrency in SocketConnectionHandler

--- src/main/java/me/experiments/websockettest/handlers/SocketConnectionHandler.java
+++ src/main/java/me/experiments/websockettest/handlers/SocketConnectionHandler.java
@@ -6,29 +6,30 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.io.IOException;
 import java.util.Map;
 import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static me.experiments.websockettest.config.Constants.MESSAGE_QUEUE_LENGTH;
 
 @Component
 public class SocketConnectionHandler extends TextWebSocketHandler {
 
-    private final Queue<WebSocketMessage<?>> messages = new LinkedList<>();
-    private final Map<String, WebSocketSession> sessions = new HashMap<>();
+    private final Queue<WebSocketMessage<?>> messages = new ConcurrentLinkedQueue<>();
+    private final AtomicInteger messageQueueSize = new AtomicInteger(0);
+    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
 
-        synchronized (sessions) {
-            for (WebSocketMessage<?> message: messages) {
-                session.sendMessage(message);
-            }
-            sessions.put(session.getId(), session);
+        for (WebSocketMessage<?> message: messages) {
+            session.sendMessage(message);
         }
+        sessions.put(session.getId(), session);
 
         System.out.println(session.getId() + " Connected");
     }
@@ -37,9 +38,8 @@ public class SocketConnectionHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
 
-        synchronized (sessions) {
-            sessions.remove(session.getId());
-        }
+        sessions.remove(session.getId());
+        session.close();
 
         System.out.println(session.getId() + " Disconnected");
     }
@@ -48,16 +48,19 @@ public class SocketConnectionHandler extends TextWebSocketHandler {
     public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
         super.handleMessage(session, message);
 
-        synchronized (sessions) {
-
-            messages.add(message);
-            if (messages.size() > MESSAGE_QUEUE_LENGTH) {
-                messages.poll();
+        messages.add(message);
+        if (messageQueueSize.incrementAndGet() > MESSAGE_QUEUE_LENGTH) {
+            if (messages.poll() != null) {
+                messageQueueSize.decrementAndGet();
             }
+        }
 
-            for (WebSocketSession otherSession: sessions.values()) {
-                if (!otherSession.getId().equals(session.getId())) {
+        for (WebSocketSession otherSession: sessions.values()) {
+            if (!otherSession.getId().equals(session.getId())) {
+                try {
                     otherSession.sendMessage(message);
+                } catch (IOException e) {
+                    System.out.println(e);
                 }
             }
         }
